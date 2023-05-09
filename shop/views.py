b875651cfed9a9dbf2db2e6d732183c5bf68e968
mhmdho <hadiomidm@gmail.com>
2023-05-09 15:54:57 +0000

    Define view of ShopList

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import ShopListSerializer
+from rest_framework import generics
 
 # Create your views here.
+
+
+class ShopListView(generics.ListAPIView):
+  queryset = Shop.objects.filter(is_confirmed=True)
+  serializer_class = ShopListSerializer
