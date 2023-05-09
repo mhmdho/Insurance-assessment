9faa33e72c5cd689fdb8dc1aea65e342d989e532
mhmdho <hadiomidm@gmail.com>
2023-05-09 18:24:08 +0000

    Define view of Register

--- user/views.py
+++ user/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import generics
+from user.models import CustomUser
+from .serializers import RegisterSerializer
 
 # Create your views here.
+
+
+class Register(generics.CreateAPIView):
+    """
+    Takes a set of informations and register user.
+    """
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        self.create(request, *args, **kwargs)
+        return Response({"Success": "Your registration was successful"}, status=status.HTTP_201_CREATED)
