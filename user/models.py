f0e72b1287eb81fa247f9ce8b69963e1fc41a6dc
mhmdho <hadiomidm@gmail.com>
2023-05-09 18:04:05 +0000

    Define CustomUser by customizing django user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+  """
+  Customize django user and add phone to it.
+  """
+  phone_regex = RegexValidator(regex=r'^09\d{9}$',
+                               message="Phone number must be entered in the format: '+989121234567'.")
+  
+  phone = models.CharField(validators=[phone_regex], max_length=11, unique=True)
+  last_login = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+      return self.phone
