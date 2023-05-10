ae025f5d7e13bc5bcdc7aa9182b84ea6240e41bc
mhmdho <hadiomidm@gmail.com>
2023-05-10 08:45:47 +0000

    Add urls of user app to the project

--- django_project/urls.py
+++ django_project/urls.py
@@ -41,4 +41,5 @@ urlpatterns = [
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('api/v1/shop/', include('shop.urls')),
+    path('api/v1/user/', include('user.urls')),
 ]
