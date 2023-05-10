73ae4dfd2118c81de54891f41a14afbae1639bef
mhmdho <hadiomidm@gmail.com>
2023-05-10 18:31:28 +0000

    Add permission class to ShopList

--- shop/views.py
+++ shop/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from .serializers import ShopListSerializer
 from rest_framework import generics
 from .models import Shop
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
 
 
 class ShopListView(generics.ListAPIView):
   queryset = Shop.objects.filter(is_confirmed=True)
+  permission_classes = (AllowAny,)
   serializer_class = ShopListSerializer
