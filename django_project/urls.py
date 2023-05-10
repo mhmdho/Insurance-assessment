1a37be6db6d23441b8687494d353b47e33c8ea77
mhmdho <hadiomidm@gmail.com>
2023-05-10 13:11:28 +0000

    Add REST framework's login and logout views urls

--- django_project/urls.py
+++ django_project/urls.py
@@ -39,6 +39,7 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api-auth/', include('rest_framework.urls')),
 
     path('api/v1/shop/', include('shop.urls')),
     path('api/v1/user/', include('user.urls')),
